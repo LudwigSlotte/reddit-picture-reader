02e4cf348af15166288bce22f8ff3410e06d86f1
Ludwig Slotte <ludwig@harbourfront.se>
2016-10-08 16:17:11 +0200

    Navigation Drawer: Added working menu, with navigation to another activity. Updated colours.

--- RedditPictureReader/app/src/main/java/se/slotte/ludwig/redditpicturereader/MainActivity.java
+++ RedditPictureReader/app/src/main/java/se/slotte/ludwig/redditpicturereader/MainActivity.java
@@ -1,10 +1,10 @@
 package se.slotte.ludwig.redditpicturereader;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import se.slotte.ludwig.redditpicturereader.favourite_pictures.FavouritePicturesActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,14 +83,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.navigation_favourite_pictures) {
+            Intent myIntent = new Intent(MainActivity.this, FavouritePicturesActivity.class);
+            MainActivity.this.startActivity(myIntent);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
