13e7c0352101233b4cf88878ed1fefb65c2f5dac
Ludwig Slotte <ludwig@harbourfront.se>
2016-10-16 18:19:59 +0200

    Main Activity: Refactor to make it easier to read.

--- RedditPictureReader/app/src/main/java/se/slotte/ludwig/redditpicturereader/MainActivity.java
+++ RedditPictureReader/app/src/main/java/se/slotte/ludwig/redditpicturereader/MainActivity.java
@@ -12,44 +12,50 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 
-import io.realm.Realm;
-import io.realm.RealmConfiguration;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import se.slotte.ludwig.redditpicturereader.favourite_pictures.FavouritePicturesActivity;
 import se.slotte.ludwig.redditpicturereader.picture_feed.PictureFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-
-    private static final String TAG = MainActivity.class.getName();
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Realm.init(this);
-        RealmConfiguration config = new RealmConfiguration.Builder()
-                .deleteRealmIfMigrationNeeded()
-                .build();
-
-        Realm.setDefaultConfiguration(config);
+        ButterKnife.bind(this);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        initToolbar();
+        initDrawerToggle();
+        initNavigationView();
+        initFragment();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+    }
+
+    private void initNavigationView() {
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initDrawerToggle() {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+    }
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        Log.d(TAG, "onCreate: MainActivity Created");
+    private void initFragment() {
         Fragment fragment = new PictureFragment();
         fragment.setRetainInstance(true);
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -59,34 +65,11 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
@@ -102,10 +85,8 @@ public class MainActivity extends AppCompatActivity
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
             emailIntent.setData(Uri.parse(getString(R.string.mail_to_data)));
             startActivity(Intent.createChooser(emailIntent, getString(R.string.menu_send_feedback)));
-
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
